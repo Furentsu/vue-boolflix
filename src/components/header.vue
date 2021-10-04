c1cbc46e0a921ea7367c3729c2ae712d3b44839d
Furentsu <eugeniofranzoso@gmail.com>
2021-10-04 16:21:49 +0200

    Initialization + working on the search feature

---
+++ src/components/header.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="container-flex">
+  <div class="row text-center mt-3">
+    <div class="col-12">
+
+      <!-- Logo -->
+      <img src="../assets/img/fakeflix.png" alt="Boolflix's logo">
+
+      <!-- Searchbar -->
+      <input v-model="search" type="text">
+      <button type="button" @click="$emit('searchInProgress', search)">Search</button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      search: '',
+    }
+},
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
