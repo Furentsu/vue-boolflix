457191635c13943c0d0d2a165285c5cf6ce73239
Furentsu <eugeniofranzoso@gmail.com>
2021-10-05 11:36:40 +0200

    Added language's flags for italian,english and american media

--- src/components/card.vue
+++ src/components/card.vue
@@ -4,33 +4,28 @@
         <h3>{{element.original_title}}</h3>
 
         <div class="language_container">
-            <img :src="element.language === 'it' ? italianFlag 
-            : element.language === 'en' ? englishFlag 
-            : element.language === 'us' ? americanFlag">
+            <img :src="element.original_language == 'it' ? require('../assets/img/italian-flag.png')
+            : element.original_language == 'en' ?  require('../assets/img/english-flag.png')
+            : element.original_language == 'us' ? require('../assets/img/usa-flag.png')
+            : ''">  
         </div>
         <h5>{{element.vote_average}}</h5>
     </div>
 </template>
 
 <script>
-import italianFlag from "../assets/img/italian-flag.png"
-import englishFlag from "../assets/img/english-flag.png"
-import americanFlag from "../assets/img/usa-flag.png"
-
 
 export default {
 name: "Card",
 props: ["element"],
-data() {
-    return {
-        italianFlag,
-        englishFlag,
-        americanFlag
-    }
-}
 }
 </script>
 
 <style lang="scss" scoped>
 
+.language_container {
+    img {
+        width: 25%;
+    }
+}
 </style>
