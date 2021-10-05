1e979579954cf25ba44cb035e7b49dd184b600b9
Furentsu <eugeniofranzoso@gmail.com>
2021-10-05 11:13:44 +0200

    Trying to display language flags under the titles

--- src/components/card.vue
+++ src/components/card.vue
@@ -2,15 +2,32 @@
     <div class="col-3 p-4 text-center">
         <h1>{{element.title}}</h1>
         <h3>{{element.original_title}}</h3>
-        <h4>{{element.original_language}}</h4>
+
+        <div class="language_container">
+            <img :src="element.language === 'it' ? italianFlag 
+            : element.language === 'en' ? englishFlag 
+            : element.language === 'us' ? americanFlag">
+        </div>
         <h5>{{element.vote_average}}</h5>
     </div>
 </template>
 
 <script>
+import italianFlag from "../assets/img/italian-flag.png"
+import englishFlag from "../assets/img/english-flag.png"
+import americanFlag from "../assets/img/usa-flag.png"
+
+
 export default {
 name: "Card",
-props: ["element"]
+props: ["element"],
+data() {
+    return {
+        italianFlag,
+        englishFlag,
+        americanFlag
+    }
+}
 }
 </script>
 
